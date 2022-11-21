f0bf6c6ba82bc125d6bf9b315c7c98dff6863b0a
mbugua <mbuguafaimba@gmail.com>
2022-11-21 14:39:04 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+var currentUser = "JOHN";
+var welcomeMessage = "Welcome to Flatbook, " + currentUser + '!'
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + '!'
+var  shortGreeting =  "Welcome, " + currentUser.charAt(0) + '!'
